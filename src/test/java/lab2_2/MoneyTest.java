7fddee86561c822493bbcacc8886db95d74b8542
Katarzyna <kaj915@wp.pl>
2015-09-18 03:51:04 +0200

    testMoneySubstract6PLN_4PLN

---
+++ src/test/java/lab2_2/MoneyTest.java
@@ -0,0 +1,19 @@
+package lab2_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class MoneyTest {
+
+	@Test
+	public void testMoneySubtract6PLN_4PLN(){
+		Money money1 = new Money(6,"PLN");
+		Money money2 = new Money(4,"PLN");
+		Money expected = new Money(2,"PLN");
+		
+		assertThat(money1.subtract(money2), is(expected));
+	}
+}
